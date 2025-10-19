c3bb294002ae76addb2ba63c8f715902f7282bfb
sean evans <sean.evans@flash.co.za>
2025-10-19 18:17:39 +0200

    Remove unwanted React/UI files from repo

--- ui/shared/src/index.js
+++
@@ -1,9 +0,0 @@
-// Export all shared components and utilities
-export { default as Sidebar } from './components/Sidebar.jsx';
-export { default as InfoCard } from './components/InfoCard.jsx';
-export { default as TransactionTable } from './components/TransactionTable.jsx';
-
-export { WalletProvider, useWallet } from './contexts/WalletContext.jsx';
-
-export * from './constants.js';
-export * from './utils.js';
